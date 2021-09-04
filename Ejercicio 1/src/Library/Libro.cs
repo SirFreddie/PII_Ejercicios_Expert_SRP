70a71d10c9380b1ffe8d19da3173693d905cc402
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-09-04 17:16:10 -0300

    Comentarios agregados

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -4,10 +4,18 @@ namespace SRP
 {
     public class Libro
     {
+        /*
+        Los datos a conocer del libro deberian ser:
+        - Titulo
+        - Autor
+        - Codigo
+        */
 
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
+
+        // El sector y el estante no deberian ser datos que el libro pueda proveer ni conocer.
         public string SectorBiblioteca { get ; set; }
         public string EstanteBiblioteca { get ; set; }
 
@@ -18,6 +26,14 @@ namespace SRP
             this.Codigo = codigo;
         }
 
+        /*
+        Considero que tanto los sectores como los estantes no deberian ser algo 
+        a ser alamacenado en esta clase.
+        Crearia una clase para el sector y otra para cada una de las estanterias.
+        La biblioteca estaria compuesta de instancias de sectores, cada instancia de sector
+        estaria compuesta por instancias de estanterias y cada estanteria estaria compuesta 
+        por instancias de libros.
+        */
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
