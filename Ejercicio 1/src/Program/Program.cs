65bd69d5b322853b8c7f6eae9b4fd766cbc9bb56
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-09-04 17:56:26 -0300

    Modificada clase Program

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -7,11 +7,26 @@ namespace SRP
     {
         static void Main(string[] args)
         {
+            Biblioteca biblioteca = new Biblioteca("Biblioteca UCU");
+            Sector sectorA = new Sector("A");
+            Sector sectorB = new Sector("B");
+
+            biblioteca.AgregarSector(sectorA);
+            biblioteca.AgregarSector(sectorB);
+
+            Estanteria estanteria7 = new Estanteria("7");
+            Estanteria estanteria6 = new Estanteria("6");
+
+            sectorA.AgregarEstanteria(estanteria7);
+            sectorB.AgregarEstanteria(estanteria6);
+
             Libro libro1 = new Libro("Design Patterns","Erich Gamma & Others","001-034");
             Libro libro2 = new Libro("Pro C#","Troelsen","001-035");
-            libro1.AlmacenarLibro("A","7");
-            libro2.AlmacenarLibro("B","3");
 
+            estanteria7.AlmacenarLibro(libro1);
+            estanteria6.AlmacenarLibro(libro2);
+
+            biblioteca.ImprimirSectores();
         }
     }
 }
